b895d76c69f0673bfa54d8d2b92620a8a7444483
jacobfisher18 <jacobfisher18@gmail.com>
2022-03-10 20:05:27 -0800

    Make connections modal state local; ability to save it all at once or cancel

--- src/components/ConnectionsModal.tsx
+++ src/components/ConnectionsModal.tsx
@@ -10,10 +10,11 @@ import {
   Divider,
 } from "@mantine/core";
 import styled from "styled-components";
-import { useConnectionsStore } from "../store/connections";
+import { Connection, useConnectionsStore } from "../store/connections";
 import { useModalsStore } from "../store/modals";
 import { isTruthy } from "../utils/nil";
 import { FiDatabase } from "react-icons/fi";
+import { useState } from "react";
 
 function AccordionLabel({
   label,
@@ -37,6 +38,126 @@ function AccordionLabel({
   );
 }
 
+function AccordionContents({
+  c,
+  handleDelete,
+  handleSave,
+}: {
+  c: Connection;
+  handleDelete: () => void;
+  handleSave: (data: Omit<Connection, "id" | "isConnected" | "type">) => void;
+}) {
+  const [name, setName] = useState(c.name);
+  const [host, setHost] = useState(c.host);
+  const [user, setUser] = useState(c.user);
+  const [password, setPassword] = useState(c.password);
+  const [port, setPort] = useState(c.port);
+  const [database, setDatabase] = useState(c.database);
+
+  return (
+    <>
+      <TextInput
+        value={name}
+        placeholder="User Database Prod"
+        label="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Row>
+        <TextInput
+          value={host}
+          placeholder="localhost"
+          label="Host"
+          onChange={(e) => setHost(e.target.value)}
+        />
+        <Space w={10} />
+        <TextInput
+          value={port}
+          placeholder="4321"
+          label="Port"
+          onChange={(e) => setPort(e.target.value)}
+        />
+      </Row>
+      <Row>
+        <TextInput
+          value={user}
+          placeholder="writer_user"
+          label="User"
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <Space w={10} />
+        <TextInput
+          type="password"
+          value={password}
+          placeholder="******"
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Row>
+      <Row>
+        <TextInput
+          value={database}
+          placeholder="main"
+          label="Database"
+          onChange={(e) => setDatabase(e.target.value)}
+        />
+        <Space w={10} />
+        <Select
+          data={["Postgres"]}
+          label="Type"
+          value={c.type}
+          disabled
+          iconWidth={0}
+          style={{ width: 180 }}
+        />
+      </Row>
+      <Group position="apart" mt={10}>
+        <Group>
+          <Button
+            variant="subtle"
+            size="xs"
+            onClick={handleDelete}
+            color="dark"
+          >
+            Delete
+          </Button>
+        </Group>
+        <Group>
+          <Button
+            variant="outline"
+            size="xs"
+            onClick={() => {
+              setName(c.name);
+              setHost(c.host);
+              setUser(c.user);
+              setPassword(c.password);
+              setPort(c.port);
+              setDatabase(c.database);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="filled"
+            size="xs"
+            onClick={() =>
+              handleSave({
+                name,
+                host,
+                user,
+                port,
+                password,
+                database,
+              })
+            }
+          >
+            Save
+          </Button>
+        </Group>
+      </Group>
+    </>
+  );
+}
+
 function ConnectionsModal() {
   const {
     isConnectionsModalOpen: isOpen,
@@ -48,6 +169,10 @@ function ConnectionsModal() {
 
   const handleAdd = () => {
     addConnection({ name: "Untitled Connection" });
+  };
+
+  const handleSave = (connectionId: string, data: Partial<Connection>) => {
+    updateConnection(connectionId, data);
   };
 
   const handleDelete = (connectionId: string) => {
@@ -80,86 +205,11 @@ function ConnectionsModal() {
                 />
               }
             >
-              <TextInput
-                value={c.name}
-                placeholder="User Database Prod"
-                label="Name"
-                onChange={(e) =>
-                  updateConnection(c.id, { name: e.target.value })
-                }
+              <AccordionContents
+                c={c}
+                handleSave={(data) => handleSave(c.id, data)}
+                handleDelete={() => handleDelete(c.id)}
               />
-              <Row>
-                <TextInput
-                  value={c.host}
-                  placeholder="localhost"
-                  label="Host"
-                  onChange={(e) =>
-                    updateConnection(c.id, { host: e.target.value })
-                  }
-                />
-                <Space w={10} />
-                <TextInput
-                  value={c.port}
-                  placeholder="4321"
-                  label="Port"
-                  onChange={(e) =>
-                    updateConnection(c.id, { port: e.target.value })
-                  }
-                />
-              </Row>
-              <Row>
-                <TextInput
-                  value={c.user}
-                  placeholder="writer_user"
-                  label="User"
-                  onChange={(e) =>
-                    updateConnection(c.id, { user: e.target.value })
-                  }
-                />
-                <Space w={10} />
-                <TextInput
-                  type="password"
-                  value={c.password}
-                  placeholder="******"
-                  label="Password"
-                  onChange={(e) =>
-                    updateConnection(c.id, { password: e.target.value })
-                  }
-                />
-              </Row>
-              <Row>
-                <TextInput
-                  value={c.database}
-                  placeholder="main"
-                  label="Database"
-                  onChange={(e) =>
-                    updateConnection(c.id, { database: e.target.value })
-                  }
-                />
-                <Space w={10} />
-                <Select
-                  data={["Postgres"]}
-                  label="Type"
-                  value={c.type}
-                  onChange={(value) =>
-                    updateConnection(c.id, { type: value ?? (null as any) })
-                  }
-                  iconWidth={0}
-                  style={{ width: 180 }}
-                />
-              </Row>
-              <Group position="apart" mt={10}>
-                <Group>
-                  <Button
-                    variant="subtle"
-                    size="xs"
-                    onClick={() => handleDelete(c.id)}
-                    color="dark"
-                  >
-                    Delete
-                  </Button>
-                </Group>
-              </Group>
             </Accordion.Item>
           ))}
       </Accordion>
